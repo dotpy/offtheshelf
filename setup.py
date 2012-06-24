4ae9b0dcea9346172e15695ad2a98e6e101ca16d
dotpy <danix@kernel-panic.it>
2012-06-24 18:53:58 +0200

    Created setup file

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+
+"""
+This is the installation script of the offtheshelf module, a very simple and
+minimal NoSQL database that uses shelve as a backend. You can run it by typing:
+
+  python setup.py install
+"""
+
+
+import sys
+from distutils.core import setup
+
+
+__author__ = "Daniele Mazzocchio <danix@kernel-panic.it>"
+__version__ = "0.0.1"
+__date__    = "Jun 24, 2012"
+
+
+# Python versions prior 2.2.3 don't support 'classifiers' and 'download_url'
+if sys.version < "2.2.3":
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
+setup(name         = "offtheshelf",
+      version      = __version__,
+      author       = "Daniele Mazzocchio",
+      author_email = "danix@kernel-panic.it",
+      packages     = ["offtheshelf"],
+      description  = "Simple NoSQL database with shelve backend",
+      classifiers  = ["Development status :: 2 - Pre-Alpha",
+                      "Environment :: Console",
+                      "Intended Audience :: Developers",
+                      "License :: OSI-Approved Open Source :: BSD License",
+                      "Natural Language :: English",
+                      "Operating System :: OS Independent",
+                      "Programming Language :: Python",
+                      "Topic :: Database"])
